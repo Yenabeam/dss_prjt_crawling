a3e3a7e780061c4e9b82e2b7060f121f57dc7f5c
alltimeno1 <cuhz108@gmail.com>
2020-12-04 18:37:09 +0900

    v0.6

--- jungonara.py
+++ jungonara.py
@@ -7,14 +7,14 @@ import pandas as pd
 import time
 import configparser
 import pymongo
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def joongonara(keyword):
     
 
     url = 'https://m.joongna.com/search-list/product?searchword={}&dateFilter=7'.format(keyword)
     options = webdriver.ChromeOptions()
-    options.add_argument("user-agent={}".format(UserAgent().chrome))
+#     options.add_argument("user-agent={}".format(UserAgent().chrome))
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36")
     options.add_argument("headless")    
     driver = webdriver.Chrome(options=options)
@@ -67,16 +67,20 @@ def joongonara(keyword):
     driver.quit()
     
     today = datetime.now()
+    delta = timedelta(days = 1)
+    today_1 = today - delta
     
     config = configparser.ConfigParser()
     config.read('/home/ubuntu/masterpiece/mongo.ini')
     mongodb_ip = config["mongo"]
-
     client = pymongo.MongoClient(mongodb_ip["ip_address"])
     db = client.joongo
     collection = db["D{}".format(today.strftime('%y%m%d'))]
     collection.insert(df)
+    client.joongo.drop_collection("D{}".format(today_1.strftime('%y%m%d')))
     
     print("Done Crawling and Insert into Mongodb")
     
     return pd.DataFrame(df)
+
+joongonara("맥북 프로")
