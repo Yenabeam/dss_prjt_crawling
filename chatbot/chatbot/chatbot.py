1eed573cc5b1e1b5e12328c90b0be017265110fc
alltimeno1 <cuhz108@gmail.com>
2020-12-09 17:53:09 +0900

    v1.0

--- chatbot/chatbot/chatbot.py
+++ chatbot/chatbot/chatbot.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response 
+from flask import Flask, request, Response
 import libs.naver as naver
 import libs.slack as slack
 import libs.fleafully as fleafully
@@ -9,22 +9,24 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 
+
 @app.route("/")
 def index():
     return "server is running!"
 
+
 @app.route("/bot", methods=['POST'])
 def bot():
-    
+
     username = request.form.get('user_name')
     token = request.form.get('token')
     text = request.form.get('text')
-    
+
     print(username, token, text)
-    
+
     # 문장 형식이 맞는지 확인
     if text.find(":") < 0:
-        
+
         if "추천" in text:
             msg = fleafully.suggest()
             slack.send_msg(Config.webhook_url, msg)
@@ -37,10 +39,10 @@ def bot():
             """
             slack.send_msg(Config.webhook_url, msg)
             return Response(), 200
-    
+
     # 명령 문자열에 따라서 코드 실행
     comm, data = text.split(":")[0], text.split(":")[1]
-      
+
     if "번역" in comm:
         msg = naver.translate(Config.naver_id, Config.naver_secret, data)
         slack.send_msg(Config.webhook_url, msg)
@@ -49,13 +51,14 @@ def bot():
         slack.send_msg(Config.webhook_url, msg)
     elif "인치" in comm:
         msg = fleafully.inch(data)
-        slack.send_msg(Config.webhook_url, msg)    
+        slack.send_msg(Config.webhook_url, msg)
     elif "지역" in comm:
         msg = fleafully.locate(data)
-        slack.send_msg(Config.webhook_url, msg)    
+        slack.send_msg(Config.webhook_url, msg)
     else:
         msg = "{}은(는)없는 명령입니다.".format(comm)
-        
+
     return Response(), 200
+
 
 app.run(debug=True)
