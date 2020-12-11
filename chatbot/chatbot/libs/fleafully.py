b9fb5697df0f372d5fac2846dec5f52c8e88c769
alltimeno1 <cuhz108@gmail.com>
2020-12-11 15:11:27 +0900

    v1.1

--- chatbot/chatbot/libs/fleafully.py
+++ chatbot/chatbot/libs/fleafully.py
@@ -30,7 +30,7 @@ def inch(size):
     today = datetime.now()
     joongo_df = pd.DataFrame(
         client.joongo["C{}R".format(today.strftime('%y%m%d%H'))].find())
-    num = joongo_df[joongo_df['inch'] == size]['inch'].count()
+    num = joongo_df[joongo_df['inch'] == str(size)]['inch'].count()
     return """
     {}인치 매물은 총 {}개입니다. :blush:
     {}
@@ -61,7 +61,7 @@ def suggest():
     good_items = joongo_df[['title', 'price', 'link', 'inch', 'year']].dropna()
     good_items[['inch', 'year']] = good_items[['inch', 'year']].astype('int')
     good_items['points'] = round(
-        good_items['price'] / ((good_items['inch'] - 10) * (good_items['year'] - 2003) ** 2))
+        good_items['price'] / ((good_items['inch'] - 10) * (good_items['year'] - 2004) ** 2))
     good_items = good_items.sort_values(by='points').reset_index(drop=True)
     return """
     오늘의 추천 매물입니다. :smile:
